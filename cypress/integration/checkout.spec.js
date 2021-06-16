d784b33091fd280f33154963b0dc92ecca327667
Ines Part <inespart@Iness-MacBook-Air.local>
2021-06-16 20:57:36 +0200

    Add timeout to Cypress test checkout.spec.js

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -9,7 +9,9 @@ describe('can proceed through checkout flow to payment page and thank you page',
     // User clicks on shopping cart icon in header
     cy.get('[data-cy="cart-icon-in-header"]').click();
     // User clicks on Checkout button
-    cy.get('[data-cy="checkout-button-on-shoppingcart-page"]').click();
+    cy.get('[data-cy="checkout-button-on-shoppingcart-page"]', {
+      timeout: 6000,
+    }).click();
     // User fills out the form on checkout page
     cy.get('[data-cy="first-name"]').type('Maria');
     cy.get('[data-cy="last-name"]').type('Musterfrau');
